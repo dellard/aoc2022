6d851eb5135404516b637920df34a4bde8da201a
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-10 03:28:52 -0500

    complete
    did something complicated and elegant,
    then ripped it out and replaced it with
    a quick hack.  It's been a long week.

--- 10.py
+++ 10.py
@@ -46,10 +46,49 @@ def part1(program, base, period):
     return total
 
 
+def part2(program, crt_width):
+
+    cycle = 1
+    sprite = 1
+
+    row_buf = []
+
+    for opcode, val in program:
+
+        if opcode == 'noop':
+            next_sprite = sprite
+            needed_cycles = 1
+        elif opcode == 'addx':
+            next_sprite = sprite + val
+            needed_cycles = 2
+
+        for _ in range(needed_cycles):
+            # just bodging something together
+            col = (cycle - 1) % crt_width
+            if col in [sprite - 1, sprite, sprite + 1]:
+                row_buf.append('#')
+            else:
+                row_buf.append(' ')
+
+            # flyback
+            if col == crt_width - 1:
+                row_buf.append('\n')
+
+            cycle += 1
+
+        sprite = next_sprite
+
+    # We might end up with an "extra" newline on the end,
+    # but all that matters for this part is that it's legible
+    #
+    return ''.join(row_buf)
+
+
 def main():
     prog = reader()
 
     print('part 1 %d' % part1(prog, 20, 40))
+    print('part 2\n%s' % part2(prog, 40))
 
 
 if __name__ == '__main__':
