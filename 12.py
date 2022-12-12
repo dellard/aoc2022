4e85cafb52ffea8647cfba13d7396d4965a7b81c
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-12 07:07:19 -0500

    cleanup; commenting
    left some diagnostics in place

--- 12.py
+++ 12.py
@@ -3,18 +3,40 @@
 import re
 import sys
 
-HUGE = 1000 * 1000
+# An impossibly long path... at least for the
+# input of this puzzle
+
+HUGE = 10000 * 10000
+
+# The BORDER sentinel value must be zero, because
+# later we use the negation of the altitudes and
+# having BORDER = 0 means that BORDER = -BORDER,
+# which gives us one less thing to worry about
+#
 BORDER = 0
 
-# I'm being lazy
+# I'm being lazy.  It would be more efficient to use
+# a queue instead of the stack, but whatever.
+#
 sys.setrecursionlimit(100000)
 
+
 def reader():
+    """
+    Read the input; return the altitude grid and
+    the start and end positions
+
+    I put a border of BORDER sentinel values around
+    the grid, so I don't have to check whether I've
+    fallen off the sides.  This means that the grid
+    doesn't have to be rectangular, and simplifies
+    some things, but might be overkill for this
+    task.
+    """
 
     grid = []
 
     row = 0
-
     for line in sys.stdin:
         row += 1
         line = line.strip()
@@ -24,8 +46,6 @@ def reader():
         if 'E' in line:
             end_pos = (row, line.find('E') + 1)
             line = re.sub('E', 'z', line)
-
-            # print('end %s' % str(end_pos))
 
         line = [BORDER] + [ord(c) for c in line] + [BORDER]
 
@@ -38,33 +58,108 @@ def reader():
 
 
 def get_grid_val(grid, pos):
+    """
+    Get the value of a grid at the given position
+
+    No bounds checking is done.
+
+    Reduces the number of times I have to type [ and ],
+    which matters to me, but really this is just a
+    minimal accessor function
+    """
+
     return grid[pos[0]][pos[1]]
 
 
 def set_grid_val(grid, pos, val):
+    """
+    Set the value of a grid at the given position
+    to the given value
+
+    No bounds checking is done.
+
+    Reduces the number of times I have to type [ and ],
+    which matters to me, but really this is just a
+    minimal accessor function
+    """
+
     grid[pos[0]][pos[1]] = val
 
 
 def find_path(grid, start_pos, end_positions):
+    """
+    Return the length of the shortest path from
+    the start position to any position in the set
+    of end positions, where the possible steps
+    from each position are as defined by the problem
+    description
+    """
 
-    rows = len(grid)
-    cols = len(grid[0])
-
+    # Make a grid of how many steps it might take to
+    # reach # each position; assume that every position
+    # is HUGEly far away
+    #
     visited = []
-    for row in range(len(grid)):
+    for _ in range(len(grid)):
         visited.append([HUGE] * len(grid[0]))
 
-    # print(grid)
-
     shortest = find_path_worker(grid, visited, start_pos, end_positions, 0)
-
-    # print(end_pos)
-    # print_visited(visited)
 
     return shortest
 
 
+def find_path_worker(
+        grid, visited, curr_pos, end_positions, path_len):
+    """
+    Resursively search for the shortest path from the current
+    position to any of the end positions and return it
+
+    Destructively fills in the visited grid with path lengths
+    that we learn as we go
+    """
+
+    # Base case: any path through the border is HUGE
+    #
+    if get_grid_val(grid, curr_pos) == BORDER:
+        return HUGE
+
+    # Base case: we already found another way to
+    # reach the current position via a path of
+    # lesser or equal length; no point in continuing
+    #
+    if path_len >= get_grid_val(visited, curr_pos):
+        return HUGE
+
+    set_grid_val(visited, curr_pos, path_len)
+
+    # Base case: we reached an end position; yay
+    #
+    if curr_pos in end_positions:
+        return 0
+
+    curr_alt = get_grid_val(grid, curr_pos)
+    row, col = curr_pos
+
+    left = row, col - 1
+    right = row, col + 1
+    up = row - 1, col
+    down = row + 1, col
+
+    candidates = []
+
+    for next_pos in [left, right, up, down]:
+        if get_grid_val(grid, next_pos) - curr_alt <= 1:
+            candidates.append(
+                    find_path_worker(
+                        grid, visited, next_pos, end_positions, path_len + 1))
+
+    return 1 + min(candidates)
+
+
 def print_visited(visited):
+    """
+    For debugging
+    """
 
     for row in visited:
         buf = ''
@@ -77,46 +172,13 @@ def print_visited(visited):
         print(buf)
 
 
-def find_path_worker(
-        grid, visited, curr_pos, end_positions, path_len):
-
-    # print('trying %s len %d' % (str(curr_pos), path_len))
-
-    if get_grid_val(grid, curr_pos) == BORDER:
-        return HUGE
-
-    if curr_pos in end_positions:
-        set_grid_val(visited, curr_pos, path_len)
-        return 0
-
-    if path_len >= get_grid_val(visited, curr_pos):
-        return HUGE
-
-    set_grid_val(visited, curr_pos, path_len)
-
-    curr_alt = get_grid_val(grid, curr_pos)
-
-    row, col = curr_pos
-    left = row, col - 1
-    right = row, col + 1
-    up = row - 1, col
-    down = row + 1, col
-
-    potential_lengths = []
-
-    for new_pos in [left, right, up, down]:
-        if get_grid_val(grid, new_pos) - curr_alt <= 1:
-            potential_lengths.append(
-                    find_path_worker(
-                        grid, visited, new_pos, end_positions, path_len + 1))
-
-    # print('potentials %s' % str(potential_lengths))
-    shortest_len = 1 + min(potential_lengths)
-
-    return shortest_len
-
-
 def find_all_as(grid):
+    """
+    Find all the grid positions that contain an 'a'
+
+    Lazily written function for a special purpose;
+    could be generalized
+    """
 
     all_as = set()
 
@@ -130,6 +192,9 @@ def find_all_as(grid):
 
 
 def main():
+    """
+    Do the puzzle for day 12
+    """
 
     grid, start, end = reader()
 
@@ -137,16 +202,17 @@ def main():
     print('part 1: %d' % shortest)
 
     # being super lazy here; instead of generalizing the
-    # searching function, I'm just going to flip the grid
-    # over and then search from the ending backwards
+    # searching function, I'm just going to invert the
+    # altitude grid and then search from the end position
+    # backwards to all the possible start positions
     #
     flipped_grid = []
     for row in grid:
         flipped_grid.append([-v for v in row])
 
-    all_as = find_all_as(grid)
-    shortest = find_path(flipped_grid, end, all_as)
+    shortest = find_path(flipped_grid, end, find_all_as(grid))
     print('part 2: %d' % shortest)
 
 
-main()
+if __name__ == '__main__':
+    main()
