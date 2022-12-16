46528fc8aca8109a61d2814fc35888072c8b6aa7
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-15 21:53:52 -0500

    added some explanation
    might come in handy if I ever decide to fix this code.

--- 15.py
+++ 15.py
@@ -1,5 +1,112 @@
 #!/usr/bin/env python3
 
+"""
+Day 15 of the 2022 Advent of Code
+
+Some comments about the algorithms here -- and things
+I tried and abandoned.
+
+This solution (particularly for part 2) doesn't satisfy
+the requirement that the solutions run in a few seconds,
+even on old and obsolete machines.  I've got a pretty
+beefy i9, and it takes about 90-100 seconds to run through
+both tests, and there's no obvious tricks to chop this
+down by an order of magnitude without having a better
+algorithm, and I don't have one.  But here's what I
+was thinking about, followed by what I did.
+
+The problem for part 2 is that you can't just build the
+grid and color in all the spaces, unless you're willing
+to wait a week for it to finish running.  So, we need to
+prune the search space as aggressively as possible, without
+losing any info.  What I attempted to do is to decrease
+the "resolution" of the cave "grid", i.e instead of having
+a 4M x 4M grid, have an N x N grid, where N is a tractable
+number like 500.  (Ideally N should divide 4M, to keep the
+arithmetic simple, but that's not strictly necessary, if
+you're careful.)  So if N is 500, then each grid element
+represents an 8000 x 8000 element of the original grid.
+Then we can scale down the coordinates for the sensors
+and beacons correspondingly, doing the rounding carefully.
+If a grid element from the N x N grid is entirely covered,
+(or entirely uncovered) then the corresponding 8k x 8k
+grid element is also covered (or uncovered), and we don't
+have to consider it again.  If the grid element from the
+N x N grid is partially covered, then we need to drill down
+closer, etc.  But the hope is that large parts of the
+original grid would be quickly categorized.
+
+It sort of worked.  It worked for the small test case,
+and a few other contrived examples.  It doesn't seem like
+a completely hopeless idea, but it had two significant
+problems:
+
+    1. Getting the scaling right is hard.  Lots of
+        detail that has to be perfect.  Not the kind
+        of code I can write in a hurry.  I don't
+        think I got it right.
+
+    2. Although it did prune down the search space,
+        it didn't prune it down *enough*.  There
+        were a lot of elements that needed to be
+        examined closely because they contained a
+        "border" between covered and uncovered
+        regions.  It cut down the search space 
+        by two orders of magnitude, more or less,
+        but I needed a lot more than that.  It would
+        take less hour or so to run, perhaps -- but
+        I never let it get very far before I
+        gave up on it.
+
+So, I abandoned this approach.  Note that this
+approach solves a more general problem than the
+one required by the task for today.  It finds
+*all* of the uncovered spots in a given region,
+however many there might turn out to be.  In some
+context, that might be necessary, but for this
+task, it ignores a very useful clue: for this
+task, there is guaranteed to be exactly ONE
+uncovered element in the region.
+
+Since we know that there's only one, that contrains
+its location a lot.  We know it can't be inside
+the area covered by a sensor -- but it must be
+immediately adjacent to two such areas, or squeezed
+between one sensor area and the border of the region
+itself.  Similar to the illustration from the
+task if we have an area as shown below, then the
+open spot can only possibly be one of the X
+positions:
+
+           X
+          X#X
+         X##BX
+        X##S##X
+         X###X
+          X#X
+           X
+
+
+So, for each sensor, compute the annulus of
+the spaces bordering the coverage area (the
+positions marked with X here).  Then find
+all of the spots that are in the intersection
+of one or more annuli, or along the border.
+[Note: I omitted this last test, because it
+never came up, due to the "only one" rule,
+which constrains where that one can be to a
+very small number of cases, which I'll leave
+as an exercise for the reader.  (hint... corners)
+
+Now you have a complete list of all of the
+areas that *might* be uncovered.  For my test
+data, this decreased the search space from
+4M x 4M to about 4.5M, which is a big help.
+Then we just have to scan through the remaining
+4.5M elements and find the first that's uncovered.
+
+"""
+
 import re
 import sys
 
@@ -89,15 +196,24 @@ def distance(pos1, pos2):
 
 def find_uncovered(sb_info, min_x, min_y, max_x, max_y):
 
+    # find all the annuli
     annuli = []
     for center, _, ext in sb_info:
         annuli.append(find_annulus(center[0], center[1], ext + 1))
 
+    # find all the positions that are in the intersections
+    # of two annuli
+    #
+    # NOTE: we don't deal with the annuli that reach the
+    # edge of the region, but that would be a good thing
+    #
     candidates = set()
     for i in range(len(annuli) - 1):
         for j in range(i + 1, len(annuli), 1):
             candidates |= annuli[i] & annuli[j]
 
+    # for each pos, see whether it is uncovered
+    #
     for pos in candidates:
         if min_x <= pos[0] <= max_x and min_y <= pos[1] <= max_y:
             missed = True
