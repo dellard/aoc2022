813be5a08ae0e6bcd46431ecec63de8cc273457c
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-15 19:28:23 -0500

    gets the right answer, but takes 100s to do it
    I must be missing something clever because this
    algorithm is going about as fast as it can go
    in Python, and it still takes a lot longer than
    it's supposed to.

--- 15.py
+++ 15.py
@@ -21,66 +21,112 @@ def find_range(sb_pairs):
     min_x, min_y = sb_pairs[0][0][0], sb_pairs[0][0][1]
     max_x, max_y = sb_pairs[0][0][0], sb_pairs[0][0][1]
 
-    new_rows = []
-    for sensor, beacon, _ in sb_pairs:
-        print('%s %s' % (str(sensor), str(min_x)))
-        min_x = min(min_x, sensor[0], beacon[0])
-        max_x = max(max_x, sensor[0], beacon[0])
+    for sensor, beacon, extent in sb_pairs:
+        # print('%s %s' % (str(sensor), str(min_x)))
+        min_x = min(min_x, sensor[0] - extent, beacon[0])
+        max_x = max(max_x, sensor[0] + extent, beacon[0])
 
-        min_y = min(min_y, sensor[1], beacon[1])
-        max_y = max(max_y, sensor[1], beacon[1])
+        min_y = min(min_y, sensor[1] - extent, beacon[1])
+        max_y = max(max_y, sensor[1] + extent, beacon[1])
 
     return (min_x, min_y), (max_x, max_y)
 
 
-def find_row_cov(sb_pairs, min_pos, max_pos, row_n):
+def find_row_cov(sb_pairs, min_pos, max_pos, row_n, inc_beacons=False):
 
-    min_x, max_x = min_pos[0], max_pos[0]
-    row_len = 1 + max_x - min_x
+    if row_n < min_pos[1] or row_n > max_pos[1]:
+        # We're outside the box
+        return []
 
-    # lazy, may regret later...
     row = {}
-    #for i in range(min_x, max_x + 1, 1):
-    #    row[i] = '.'
 
     for sensor, beacon, extent in sb_pairs:
         projection = extent - abs(sensor[1] - row_n)
         if projection > 0:
-            print('ADD ', sensor, beacon, extent)
-            for i in range(sensor[0] - projection, 1 + sensor[0] + projection, 1):
-                row[i] = '#'
+            start_x = max(sensor[0] - projection, min_pos[0])
+            end_x = min(1 + sensor[0] + projection, 1 + max_pos[0])
 
-            # print('R ' + ''.join([row[i] for i in range(min_x, max_x + 1, 1)]))
+            for i in range(start_x, end_x, 1):
+                if min_pos[0] <= i <= max_pos[0]:
+                    row[i] = '#'
 
-    # Now add the beacons back in
+    # Now replace any actual beacons
+    #
+    # Not an effcient way to do this, alas
+    #
     for sensor, beacon, extent in sb_pairs:
         if beacon[1] == row_n:
             row[beacon[0]] = 'B'
 
-    count = 0
+    offsets = []
     for ind, val in row.items():
-        if val == '#':
-            count += 1
-        else:
-            print('nah at %d' % ind)
+        if inc_beacons or val == '#':
+            offsets.append(ind)
 
-    return count
+    return offsets
+
+
+def find_annulus(center_x, center_y, radius):
+
+    if radius == 0:
+        return [(center_x, center_y)]
+
+    annulus = set()
+
+    for i in range(radius):
+        annulus.add((center_x + i, center_y - (radius - i)))
+        annulus.add((center_x + radius - i, center_y + i))
+        annulus.add((center_x - i, center_y + (radius - i)))
+        annulus.add((center_x - (radius - i), center_y - i))
+
+    return annulus
+
+
+def distance(pos1, pos2):
+
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+
+def find_uncovered(sb_info, min_x, min_y, max_x, max_y):
+
+    annuli = []
+    for center, _, ext in sb_info:
+        annuli.append(find_annulus(center[0], center[1], ext + 1))
+
+    candidates = set()
+    for i in range(len(annuli) - 1):
+        for j in range(i + 1, len(annuli), 1):
+            candidates |= annuli[i] & annuli[j]
+
+    for pos in candidates:
+        if min_x <= pos[0] <= max_x and min_y <= pos[1] <= max_y:
+            missed = True
+            for center, _, ext in sb_info:
+                if distance(center, pos) <= ext:
+                    missed = False
+                    break
+
+            if missed:
+                return pos
+
+    return None
 
 
 def main():
 
-    rows = reader()
+    sb_info = reader()
 
-    for row in rows:
-        print(row)
+    min_pos, max_pos = find_range(sb_info)
+    covered = find_row_cov(sb_info, min_pos, max_pos, 2000000)
 
-    min_pos, max_pos = find_range(rows)
+    print('part 1: %d' % len(covered))
 
-    print(find_row_cov(rows, min_pos, max_pos, 2000000))
-    # print(min_pos, max_pos)
+    uncovered = find_uncovered(sb_info, 0, 0, 4000000, 4000000)
+    if uncovered:
+        print('part 2: %d' % (uncovered[0] * 4000000 + uncovered[1]))
+    else:
+        print('part 2: DISASTER')
 
 
 if __name__ == '__main__':
     main()
-
-
