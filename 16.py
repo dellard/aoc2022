386e94b4c935c80626d8427708bb3c5a3a5a08cd
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-18 17:55:00 -0500

    still off by a few.  argh

--- 16.py
+++ 16.py
@@ -222,7 +222,11 @@ def extend2(cave, node0, dist0, node1, dist1, path, current_total, minutes):
                 if new_n1 in path:
                     continue
 
-                warp = min(new_d0, new_d1)
+                if new_d0 > new_d1:
+                    new_n0, new_n1 = new_n1, new_n0
+                    new_d0, new_d1 = new_d1, new_d0
+
+                warp = min(new_d0 + 1, new_d1 + 1)
 
                 ext_total, ext_path = extend2(
                         cave,
@@ -230,13 +234,22 @@ def extend2(cave, node0, dist0, node1, dist1, path, current_total, minutes):
                         new_n1, new_d1 - warp,
                         path + [new_n0, new_n1],
                         new_total,
-                        minutes - (warp + 1))
+                        minutes - warp)
+                        #minutes - (warp + 1))
 
                 if highest_total < ext_total:
                     highest_total, highest_path = ext_total, ext_path
 
-    if dist0 == 0 and dist1 != 0:
-        print('CASE 0 X')
+    if dist0 == 0 or dist1 == 0:
+
+        # reorder things so that name0/dist0 is always the zero
+        #
+        if dist1 == 0:
+            print('REORDER')
+            node0, node1 = node1, node0
+            dist0, dist1 = dist1, dist0
+
+        print('CASE X 0')
         increase = cave.valves[node0].rate
         new_total = base_total + (increase * (minutes - 1))
 
@@ -261,58 +274,24 @@ def extend2(cave, node0, dist0, node1, dist1, path, current_total, minutes):
             if new_neighbor in path:
                 continue
 
-            warp = min(new_distance, dist1 - 1)
+            #warp = min(new_distance + 1, dist1)
+            warp = min(new_distance + 1, dist1)
             ext_total, ext_path = extend2(
                     cave,
                     new_neighbor, new_distance - warp,
                     node1, dist1 - warp,
                     path + [new_neighbor],
                     new_total,
-                    minutes - (warp + 1))
+                    minutes - warp)
+                    # minutes - (warp + 1))
 
             if highest_total < ext_total:
                 highest_total, highest_path = ext_total, ext_path
 
-    if dist0 != 0 and dist1 == 0:
-        print('CASE X 0')
-        increase = cave.valves[node1].rate
-        new_total = base_total + (increase * (minutes - 1))
-
-        if len(path) == len(cave.paths):
-            print('ALL OUT X 0')
-            return new_total, path
-
-        neighbors = cave.paths[node1]
-        for new_neighbor, new_distance in neighbors:
-            # If the neighbor is too far away to open
-            # in time, then don't bother
-            #
-            if new_distance >= minutes:
-                continue
-
-            # If the neighbor is already on our path,
-            # then we can't open it again
-            # (we might pass through it on our way to
-            # another node, but that's already accounted
-            # for in the distance of the paths)
-            #
-            if new_neighbor in path:
-                continue
-
-            warp = min(dist0 - 1, new_distance)
-            ext_total, ext_path = extend2(
-                    cave,
-                    node0, dist0 - 1,
-                    new_neighbor, new_distance,
-                    path + [new_neighbor],
-                    new_total,
-                    minutes - (warp + 1))
-
-            if highest_total < ext_total:
-                highest_total, highest_path = ext_total, ext_path
-
+    # This shouldn't happen, if we're doing things correctly.
+    #
     if dist0 > 0 and dist1 > 0:
-        print('CASE X X')
+        print('OOPS CASE X X')
         ext_total, ext_path = extend2(
                 cave,
                 node0, dist0 - 1,
