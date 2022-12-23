86d466459614e33b9cd595dd223e3ea91ef06386
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-23 09:02:30 -0500

    some neatening up

--- 23.py
+++ 23.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+"""
+Advent of Code 2022, day 23
+"""
+
 import sys
 
 
 def reader():
 
-    rows = []
     elves = set()
 
     rownum = 0
@@ -21,57 +24,66 @@ def reader():
 
 
 def find_extent(positions):
+    """
+    Find the extents of the current elf "rectangle"
+    and the area (occupied or unoccupied) of this rectangle
+    """
 
-    unset = True
-    for x_pos, y_pos in positions:
-        if unset:
-            unset = False
-            x_min, y_min = x_pos, y_pos
-            x_max, y_max = x_pos, y_pos
-            continue
+    if not positions:
+        # This should never happen
+        #
+        return 0, (0, 0), (0, 0)
 
-        if x_min > x_pos:
-            x_min = x_pos
-        if x_max < x_pos:
-            x_max = x_pos
-        if y_min > y_pos:
-            y_min = y_pos
-        if y_max < y_pos:
-            y_max = y_pos
+    x_min = min(x_pos for x_pos, y_pos in positions)
+    y_min = min(y_pos for x_pos, y_pos in positions)
+    x_max = max(x_pos for x_pos, y_pos in positions)
+    y_max = max(y_pos for x_pos, y_pos in positions)
 
     area = (1 + x_max - x_min) * (1 + y_max - y_min)
     return area, (x_min, y_min), (x_max, y_max)
 
 
+# You have entered a region of dense copy/pasting.
+
 def move_n(pos):
     return pos[0], pos[1] - 1
+
 
 def move_e(pos):
     return pos[0] + 1, pos[1]
 
+
 def move_s(pos):
     return pos[0], pos[1] + 1
+
 
 def move_w(pos):
     return pos[0] - 1, pos[1]
 
+
 def move_ne(pos):
     return move_n(move_e(pos))
+
 
 def move_nw(pos):
     return move_n(move_w(pos))
 
+
 def move_se(pos):
     return move_s(move_e(pos))
+
 
 def move_sw(pos):
     return move_s(move_w(pos))
 
+
 def occ(elves, pos):
     return pos in elves
 
+
 def unocc(elves, pos):
     return pos not in elves
+
 
 def check_n(elves, pos):
     if all([
@@ -82,6 +94,7 @@ def check_n(elves, pos):
     else:
         return None
 
+
 def check_s(elves, pos):
     if all([
             unocc(elves, move_se(pos)),
@@ -90,6 +103,7 @@ def check_s(elves, pos):
         return move_s(pos)
     else:
         return None
+
 
 def check_e(elves, pos):
     if all([
@@ -100,6 +114,7 @@ def check_e(elves, pos):
     else:
         return None
 
+
 def check_w(elves, pos):
     if all([
             unocc(elves, move_nw(pos)),
@@ -109,16 +124,17 @@ def check_w(elves, pos):
     else:
         return None
 
+
 def neighbor_count(elves, pos):
 
     p_x, p_y = pos
 
     cnt = 0
-    for x in range(p_x - 1, p_x + 2, 1):
-        for y in range(p_y - 1, p_y + 2, 1):
-            if x == p_x and y == p_y:
+    for e_x in range(p_x - 1, p_x + 2, 1):
+        for e_y in range(p_y - 1, p_y + 2, 1):
+            if e_x == p_x and e_y == p_y:
                 continue
-            if (x, y) in elves:
+            if (e_x, e_y) in elves:
                 cnt += 1
 
     return cnt
@@ -126,29 +142,32 @@ def neighbor_count(elves, pos):
 
 def round_step1(elves, round_number):
 
-    move_order = [
-            check_n, check_s, check_w, check_e
-        ]
-    test_order = [0, 1, 2, 3]
+    move_order = [check_n, check_s, check_w, check_e]
 
     # pure laziness
     n_directions = len(move_order)
     base = round_number % n_directions
     move_order += move_order
-    test_order += test_order
 
     move_order = move_order[base:base + n_directions]
-    test_order = test_order[base:base + n_directions]
-    # print('TEST ORDER ', test_order)
-    # print('moves %d %d %s' % (base, round_number, move_order))
 
+    # spots is a pair of positions: where each elf is,
+    # and where it wants to go
+    # cnts is the number of elves who want to go to
+    # each position (for positions any elves actually
+    # want to go to)
+    #
     proposed_spots = set()
     proposed_cnts = {}
 
     for pos in elves:
         if neighbor_count(elves, pos) == 0:
+            # No neighbors?  Happily stay where you are.
             new_pos = pos
         else:
+            # Try moving in each direction until you find
+            # one that works, or give up in despair
+            #
             for move in move_order:
                 new_pos = move(elves, pos)
                 if new_pos:
@@ -174,6 +193,9 @@ def round_step1(elves, round_number):
 
 
 def print_elves(elves):
+    """
+    For debugging: print the positions of the elves
+    """
 
     _, min_pos, max_pos = find_extent(elves)
 
@@ -181,9 +203,9 @@ def print_elves(elves):
     max_x, max_y = max_pos
 
     buf = []
-    for y in range(min_y, max_y + 1, 1):
-        for x in range(min_x, max_x + 1, 1):
-            if (x, y) in elves:
+    for e_y in range(min_y, max_y + 1, 1):
+        for e_x in range(min_x, max_x + 1, 1):
+            if (e_x, e_y) in elves:
                 buf.append('#')
             else:
                 buf.append('.')
@@ -198,10 +220,7 @@ def part1(elves):
     for rnd in range(10):
         elves = round_step1(elves, rnd)
 
-    area, min_p, max_p = find_extent(elves)
-    # print('min_p ', min_p)
-    # print('max_p ', max_p)
-    # print('occupied ', len(elves))
+    area, _, _ = find_extent(elves)
     return area - len(elves)
 
 
@@ -210,14 +229,13 @@ def part2(elves):
     rnd = 0
     while True:
         new_elves = round_step1(elves, rnd)
-        rnd += 1
-
         if elves == new_elves:
             break
 
         elves = new_elves
+        rnd += 1
 
-    return rnd
+    return 1 + rnd
 
 
 def main():
