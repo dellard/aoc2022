47bd36f0e3a0bd8fa08bbf7bae37490b58d6a8a7
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-27 10:40:53 -0500

    tidying up day 4

--- 04.py
+++ 04.py
@@ -1,50 +1,63 @@
 #!/usr/bin/env python3
 
+"""
+Advent of Code 2022, day 4
+"""
+
 import sys
 
-def is_within(p0, p1):
 
-    return (p0[0] >= p1[0] and p0[0] <= p1[1] and
-            p0[1] >= p1[0] and p0[1] <= p1[1])
+def is_within(point0, point1):
+
+    return (point0[0] >= point1[0] and point0[0] <= point1[1] and
+            point0[1] >= point1[0] and point0[1] <= point1[1])
 
 
-def does_overlap(p0, p1):
+def does_overlap(point0, point1):
 
     # Feelin' lazy
     #
-    if p0[0] >= p1[0] and p0[0] <= p1[1]:
+    if point0[0] >= point1[0] and point0[0] <= point1[1]:
         result = True
-    elif p0[1] >= p1[0] and p0[1] <= p1[1]:
+    elif point0[1] >= point1[0] and point0[1] <= point1[1]:
         result = True
-    elif p1[0] >= p0[0] and p1[0] <= p0[1]:
+    elif point1[0] >= point0[0] and point1[0] <= point0[1]:
         result = True
-    elif p1[1] >= p0[0] and p1[1] <= p0[1]:
+    elif point1[1] >= point0[0] and point1[1] <= point0[1]:
         result = True
     else:
         result = False
 
-    # print('%s  %s / %s' % (result, p0, p1))
     return result
 
 
-count0 = 0
-count1 = 0
+def main():
+    count0 = 0
+    count1 = 0
 
-for line in sys.stdin:
-    r0_s, r1_s = line.strip().split(',')
+    for line in sys.stdin:
 
-    r00_s, r01_s = r0_s.split('-')
-    r10_s, r11_s = r1_s.split('-')
+        # This method of parsing the input is more
+        # cautious than necessary for AoC
+        #
+        r0_s, r1_s = line.strip().split(',')
 
-    p0 = int(r00_s), int(r01_s)
-    p1 = int(r10_s), int(r11_s)
+        r00_s, r01_s = r0_s.split('-')
+        r10_s, r11_s = r1_s.split('-')
 
-    if is_within(p0, p1) or is_within(p1, p0):
-        count0 += 1
+        point0 = int(r00_s), int(r01_s)
+        point1 = int(r10_s), int(r11_s)
 
-    if does_overlap(p0, p1):
-        count1 += 1
+        if is_within(point0, point1) or is_within(point1, point0):
+            count0 += 1
 
-print('test0: total %d' % count0)
-print('test1: total %d' % count1)
+        if does_overlap(point0, point1):
+            count1 += 1
+
+    print('part 1: %d' % count0)
+    print('part 2: %d' % count1)
+
+
+if __name__ == '__main__':
+    main()
 
