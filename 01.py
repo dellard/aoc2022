0f3a7f45e7cc6731bd40f3c00f16de1e1376ab6b
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-27 08:47:18 -0500

    tidy up day 1

--- 01.py
+++ 01.py
@@ -1,25 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Advent of Code 2022, day 1
+"""
 
 import sys
 
-totals = list()
 
-current_sum = 0
+def main():
+    """
+    Advent of Code 2022, day 1
+    """
 
-for l in sys.stdin:
+    totals = []
 
-    l = l.strip()
-    if not l:
+    current_sum = 0
+
+    for line in sys.stdin:
+
+        line = line.strip()
+        if not line:
+            totals.append(current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(line)
+
+    # deal with the last group, which ends at the end
+    # of the input, not a blank line
+    #
+    if current_sum > 0:
         totals.append(current_sum)
-        current_sum = 0
-    else:
-        current_sum += int(l)
 
-# deal with any leftovers
-if current_sum > 0:
-    totals.append(current_sum)
+    totals = sorted(totals)
+    print('part 1: %d' % totals[-1])
+    print('part 2: %d' % sum(totals[-3:]))
 
-# For the first part:
-totals = sorted(totals)
 
-print('Part A: %d' % totals[-1])
-print('Part B: %d' % sum(totals[-3:]))
+if __name__ == '__main__':
+    main()
