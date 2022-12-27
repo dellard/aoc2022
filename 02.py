c2c06f66f7f635d5b08002b0674f4b2dc8fef8eb
Daniel Ellard <dan.ellard@raytheon.com>
2022-12-27 08:54:22 -0500

    tidying up day 2

--- 02.py
+++ 02.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
 
+"""
+Advent of Code 2022, day 2
+"""
+
 import sys
 
-OUTCOMES1 = {
-        'A X' : 1 + 3,
-        'B X' : 1 + 0,
-        'C X' : 1 + 6,
-        'A Y' : 2 + 6,
-        'B Y' : 2 + 3,
-        'C Y' : 2 + 0,
-        'A Z' : 3 + 0,
-        'B Z' : 3 + 6,
-        'C Z' : 3 + 3,
-}
 
-OUTCOMES2 = {
-        'A X' : 3 + 0,
-        'B X' : 1 + 0,
-        'C X' : 2 + 0,
-        'A Y' : 1 + 3,
-        'B Y' : 2 + 3,
-        'C Y' : 3 + 3,
-        'A Z' : 2 + 6,
-        'B Z' : 3 + 6,
-        'C Z' : 1 + 6,
-}
+def main():
+    """
+    Advent of Code 2022, day 2
+    """
 
-SCORE1 = 0
-SCORE2 = 0
+    # for part 1
+    score1 = 0
+    outcomes1 = {
+            'A X' : 1 + 3,
+            'B X' : 1 + 0,
+            'C X' : 1 + 6,
+            'A Y' : 2 + 6,
+            'B Y' : 2 + 3,
+            'C Y' : 2 + 0,
+            'A Z' : 3 + 0,
+            'B Z' : 3 + 6,
+            'C Z' : 3 + 3,
+    }
 
-for line in sys.stdin:
-    line = line.strip()
+    # for part 2
+    score2 = 0
+    outcomes2 = {
+            'A X' : 3 + 0,
+            'B X' : 1 + 0,
+            'C X' : 2 + 0,
+            'A Y' : 1 + 3,
+            'B Y' : 2 + 3,
+            'C Y' : 3 + 3,
+            'A Z' : 2 + 6,
+            'B Z' : 3 + 6,
+            'C Z' : 1 + 6,
+    }
 
-    if line not in OUTCOMES1:
-        print('oops1 %s' % line)
-    if line not in OUTCOMES2:
-        print('oops2 %s' % line)
+    for line in sys.stdin:
+        line = line.strip()
+
+        score1 += outcomes1[line]
+        score2 += outcomes2[line]
+
+    print('part 1: %d' % score1)
+    print('part 2: %d' % score2)
 
 
-    SCORE1 += OUTCOMES1[line]
-    SCORE2 += OUTCOMES2[line]
-
-print('task 1: total score %d' % SCORE1)
-print('task 2: total score %d' % SCORE2)
-
-
-
+if __name__ == '__main__':
+    main()
